83cc45119d00ebf435effa8fcda404fd45185b76
zeebaby193 <zainabenitan422@gmail.com>
2026-02-19 22:47:38 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+//Create a variable for your name, age, your favorite number and whether you like coding (true/false). Then print them using Console.WriteLine().
+
+string name = "Henny";
+int age = 12;
+decimal number = 23.5m;
+bool likeCoding = true;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(number);
+Console.WriteLine(likeCoding);
