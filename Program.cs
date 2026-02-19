d801b044afbb8b12899cde41a3072c63015dfecb
zeebaby193 <zainabenitan422@gmail.com>
2026-02-19 23:13:06 +0100

    Add more information

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿
 //Create a variable for your name, age, your favorite number and whether you like coding (true/false). Then print them using Console.WriteLine().
 
-string name = "Henny";
+/*string name = "Henny";
 int age = 12;
 decimal number = 23.5m;
 bool likeCoding = true;
@@ -9,4 +9,23 @@ bool likeCoding = true;
 Console.WriteLine(name);
 Console.WriteLine(age);
 Console.WriteLine(number);
-Console.WriteLine(likeCoding);
+Console.WriteLine(likeCoding);*/
+
+
+//Write a program that contains this 4 things.
+//1. Have at least 3 comments
+//2. Declare at least 4 variables
+//3. Use correct syntax
+//4. Print all variables using Console.WriteLine().
+
+Console.WriteLine("========Welcome to My Profile========");
+string name = "Zeebaby";
+int age = 12;
+double number = 5;
+bool codingIsFun = true;
+
+
+Console.WriteLine($"My name is {name}, I am {age} years old, My favorite number is {number}, And i like Coding: {codingIsFun}.");
+
+Console.WriteLine("==========Thanks for viewing my Page===========");
+
